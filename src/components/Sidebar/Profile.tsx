5ffcab39403eb2f11562fa2a989c5b9a732320c2
EsterZanato <esterzanatocb@gmail.com>
2024-07-26 07:24:41 -0300

    atualização

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,16 +1,21 @@
 import { LogOut } from "lucide-react";
+import Image from "next/image";
 
 export function Profile() {
-    return(
-        <div className="grid grid-cols-profile items-center gap-3">
-          <img src="https://github.com/Felipalds.png" className="h-10 w-10 rounded-full" alt="" />
-          <div className="flex flex-col truncate">
-            <span className="text-sm font-semibold text-zinc-700">Luiz Felipe</span>
-            <span className="truncate text-sm text-zinc-500">example@hotmail.com</span>
-            </div> 
-            <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
-                <LogOut className="h-5 w-5 text-zinc-500" />
-            </button>    
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <div className="relative h-10 w-10 rounded-full overflow-hidden">
+        <Image src="https://github.com/EsterZanato.png" fill alt="Profile" />
+      </div>
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">Ester Zanato</span>
+        <span className="truncate text-sm text-zinc-500">
+          example@hotmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  );
 }
