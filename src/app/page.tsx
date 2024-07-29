19b702b44d7d534acc5ccec597a88097e654df86
EsterZanato <esterzanatocb@gmail.com>
2024-07-29 15:23:58 -0300

    FileInput component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SettingsTabs } from '@/components/SettingsTabs'
 import * as Input from '@/components/Input'
-import { Mail, UploadCloud, User } from 'lucide-react'
+import { Mail } from 'lucide-react'
+import * as FileInput from '@/components/Form/FileInput'
 
 export default function Home() {
   return(
@@ -67,26 +68,12 @@ export default function Home() {
               </span>
               </label>
               
-              <div className='flex items-start gap-5'>
-                <div className='bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full'>
-                  <User className='w-8 h-8 text-violet-500' /> 
-                </div>
+                <FileInput.Root className='flex items-start gap-5'>
+                  <FileInput.ImagePreview />
+                  <FileInput.Trigger />
+                  <FileInput.Control />     
+                </FileInput.Root>
 
-                <label htmlFor="photo" className='group flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25 hover:text-violet-500'>
-                <span className='rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100'>
-                  <UploadCloud className='h-5 -5 text-zinc-600 group-hover:text-violet-600' />
-                </span>
-
-                <div className='flex flex-col items-center gap-1'>
-                  <span className='text-sm'>
-                   <span className='font-semibold text-violet-700'>Click to upload </span>
-                    or drag and drop</span>
-                  <span className='text-sm'>SVG, PNG, JPG, or GIF (max. 800x400px)</span>
-                </div>
-                </label>
-
-                <input type="file" className='sr-only' id='photo' />
-              </div>
             </div>
 
             <div className='grid gap-3 grid-cols-form pt-5'>
@@ -133,8 +120,11 @@ export default function Home() {
               Share a few snippets of your work.
               </span>
               </label>
-              
-              <div></div>
+              <FileInput.Root>
+                  <FileInput.Trigger />
+                  <FileInput.Control multiple />     
+                </FileInput.Root>
+
             </div>
 
             <div className='flex items-center justify-end gap-2 pt-5'>
---
+++ src/components/Form/FileInput/Root.tsx
@@ -0,0 +1,8 @@
+import { ComponentProps } from "react"
+
+export type RootProps = ComponentProps<'div'>
+
+
+export function Root(props: RootProps) {
+    return <div {...props} />
+}
